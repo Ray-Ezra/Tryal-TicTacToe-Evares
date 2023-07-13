730c96c1bb5243483cc4d13846ca6c0949cf5f93
raymond <raymondezra276@gmail.com>
2023-07-13 17:21:37 +0300

    Updated comments

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
+//used for  importing the Mongoose library, 
 const mongoose = require("mongoose");
 
+//Whole function used  to connect to Mongo
 const connectDB = async () => {
   try {
+// Ensures to wait for the connection to be established
     await mongoose.connect("mongodb+srv://victorkimaru8:stvick.9028@cluster0.3clz1wb.mongodb.net/TicTacToe?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+//If the connection is successful message is returned
     console.log("MongoDB connected");
+//If the connection is unsuccessful message is returned
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
   }
